4a637cb1fc4b4dd5b8bdd6ac5631bcef75074dce
mustazir <billah15-5193@diu.edu.bd>
2025-07-06 04:12:54 +0600

    Error Handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { bookRoutes } from "../controller/book.controller";
 
 
@@ -15,5 +15,25 @@ app.use("/books",bookRoutes); // /notes is the rote then redirect to noteRoutes 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Note App!");
 });
+
+// Global error handler && always cretae the samme structure for error handler 1st the 404 error handler for rote not and the the other error handler 500 for all error 
+
+app.use((req:Request,res:Response,next:NextFunction) => {
+  res.status(404).json({
+    message: "Route not found"
+    
+  })
+})
+
+app.use((error:any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    console.error("Error: from gg", error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+
+  }
+})
 
 export default app;
--- src/controller/book.controller.ts
+++ src/controller/book.controller.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Book } from './../models/books.model';
 
 
 export const bookRoutes = express.Router();
 
 
-bookRoutes.post("/create-Book",async (req: Request, res: Response) => { 
+bookRoutes.post("/books",async (req: Request, res: Response,next:NextFunction) => { 
     try {
         const body = req.body;
         const book = await Book.create(body);
@@ -14,22 +14,25 @@ bookRoutes.post("/create-Book",async (req: Request, res: Response) => {
             book
         });
     } catch (error) {
-        res.status(500).json({
-            message: error.message,
-            error,
-            
-        });
-        console.log(error);
+        next(error); // Pass the error to the global error handler
+       
     }
 })
 
-bookRoutes.get("/",async (req :Request,res :Response)=>{
-    const body= req.body
+bookRoutes.get("/books",async (req :Request,res :Response,next:NextFunction)=>{
+   try {
+     const body= req.body
 
     const books =await Book.find();
     res.status(200).json({
         books
     });
+   } catch (error) {
+    next(error); 
+   }
 
 })
+
+
+
 
